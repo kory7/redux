e2c3e284dbac189f969d0643761c7e03c1500e9a
Jony choi <jony.choi7@gmail.com>
2018-03-05 01:13:02 -0600

    Capitulo 15

---
+++ src/components/search_bar.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {term:''};
+    }
+    render(){
+        return <input onChange={e => this.setState({term: e.target.value})}/>
+    }
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+ReactDOM.render(<SearchBar />, document.querySelector('.container'));
